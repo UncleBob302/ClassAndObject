cd6e07d4789c1e6436b32204fe01d123aba1b947
UncleBob302 <iqbalgavi46@gmail.com>
2019-05-01 19:33:16 +0700

    class object project

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Mahasiswa
+    {
+        // properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        // method
+        public void registrasi()
+        {
+            Console.WriteLine("Nama; {0}", Nama);
+            Console.WriteLine("Nim; {0}", Nim);
+            Console.WriteLine("Ipk; {0}", Ipk);
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses krs\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "Kamaruddin";
+            saya.Ipk = 4;
+
+            kamu.Nim = "22834";
+            kamu.Nama = "Iqbal";
+            kamu.Ipk = 3.80f;
+
+            // memanggil method registrasi dan isi krs
+            saya.registrasi();
+            saya.IsiKrs();
+
+            kamu.registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
